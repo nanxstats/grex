21800edd200ddf62ed74a8f2d828c683fec2c672
Nan Xiao <road2stat@gmail.com>
2019-01-27 14:10:15 -0500

    update roxygen2

--- R/grex-package.R
+++ R/grex-package.R
@@ -1,18 +1,2 @@
-#' Gene ID Mapping for Genotype-Tissue Expression (GTEx) Data
-#'
-#' Gene ID Mapping for Genotype-Tissue Expression (GTEx) Data
-#'
-#' Opened the vignette via \code{vignette("grex")}.
-#'
-#' \tabular{ll}{
-#' Package: \tab grex\cr
-#' Type: \tab Package\cr
-#' License: \tab GPL-3}
-#'
-#' @name grex-package
-#' @docType package
-#' @author
-#' Nan Xiao <\email{me@@nanx.me}>,
-#' Gao Wang <\email{gaow@@uchicago.edu}>,
-#' Lei Sun <\email{sunl@@uchicago.edu}>
-NULL
+#' @keywords internal
+"_PACKAGE"
